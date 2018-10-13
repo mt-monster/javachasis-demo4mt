1c2fbd2b6100fad14e4a185759b6048806d4d7aa
m00416667 <mengtao4@huawei.com>
2018-10-13 23:55:54 +0800

    nio test

---
+++ test-pojo-provider/src/main/java/com/huawei/demo4mt/NioPakage/Calculator.java
@@ -0,0 +1,20 @@
+package com.huawei.demo4mt.NioPakage;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
+/**
+ * 一句话功能简述
+ * 功能详细描述
+ * @author m00416667
+ * @version [版本号, ]
+ * @see  [相关类/方法]
+ * @since [产品/模块版本]
+ * Package Name:com.huawei.demo4mt.NioPakage
+ */
+public final class Calculator {
+   public static final ScriptEngine jse= new ScriptEngineManager().getEngineByName("JavaScript");
+   public static Object cal(String expression) throws Exception{
+       return jse.eval(expression);
+   }
+}
---
+++ test-pojo-provider/src/main/java/com/huawei/demo4mt/NioPakage/ServerHandler.java
@@ -0,0 +1,59 @@
+package com.huawei.demo4mt.NioPakage;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * 一句话功能简述
+ * 功能详细描述
+ * @author m00416667
+ * @version [版本号, ]
+ * @see  [相关类/方法]
+ * @since [产品/模块版本]
+ * Package Name:com.huawei.demo4mt.NioPakage
+ */
+public class ServerHandler implements Runnable {
+
+    private Socket socket;
+
+    public ServerHandler(Socket socket) {
+        this.socket = socket;
+    }
+
+    /**
+     * When an object implementing interface <code>Runnable</code> is used
+     * to create a thread, starting the thread causes the object's
+     * <code>run</code> method to be called in that separately executing
+     * thread.
+     * <p>
+     * The general contract of the method <code>run</code> is that it may
+     * take any action whatsoever.
+     *
+     * @see     Thread#run()
+     */
+    @Override public void run() {
+        System.out.println("serverhandler is coming....");
+        BufferedReader in;
+        PrintWriter out;
+        try {
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
+            String expression;
+            String result;
+
+            while (true) {
+                if ((expression = in.readLine()) == null) {
+                    break;
+                }
+                System.out.println("服务器收到消息" + expression);
+                Calculator.cal(expression);
+
+            }
+
+        } catch (Exception e) {
+        }
+
+    }
+}
