736de5c5170a1dbb1d4f85ba7011524155c84bd0
m00416667 <mengtao4@huawei.com>
2019-01-19 15:55:35 +0800

    fix some error

--- test-pojo-provider/src/main/java/com/huawei/demo4mt/aiopkg/AccpetHandler.java
+++ test-pojo-provider/src/main/java/com/huawei/demo4mt/aiopkg/AccpetHandler.java
@@ -11,22 +11,22 @@ import java.nio.channels.CompletionHandler;
  * @version [版本号, ]
  * @see  [相关类/方法]
  * @since [产品/模块版本]
- * Package Name:com.huawei.demo4mt.aiopkg
+ * Package Name:com.huawei.demo4mt.AioPkg
  */
 public class AccpetHandler implements CompletionHandler<AsynchronousSocketChannel, AsyncServerHandler> {
-    @Override public void completed(AsynchronousSocketChannel socketChannel, AsyncServerHandler serverHandler) {
+  @Override
+  public void completed(AsynchronousSocketChannel socketChannel, AsyncServerHandler serverHandler) {
 
-        AioServer.clientcount++;
-        System.out.println("连接的客户端数： " + AioServer.clientcount);
-        serverHandler.serverSocketChannel.accept(serverHandler, this);
-        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
-        socketChannel.read(byteBuffer, byteBuffer, new ReadHandler(socketChannel));
+    AioServer.clientcount++;
+    System.out.println("连接的客户端数： " + AioServer.clientcount);
+    serverHandler.serverSocketChannel.accept(serverHandler, this);
+    ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+    socketChannel.read(byteBuffer, byteBuffer, new ReadHandler(socketChannel));
+  }
 
-    }
-
-    @Override public void failed(Throwable exc, AsyncServerHandler serverHandler) {
-        exc.printStackTrace();
-        serverHandler.latch.countDown();
-    }
-
+  @Override
+  public void failed(Throwable exc, AsyncServerHandler serverHandler) {
+    exc.printStackTrace();
+    serverHandler.latch.countDown();
+  }
 }
